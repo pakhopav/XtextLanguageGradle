fed6d68ca5120fc09d6c113f113531c2bd93d4fd
Pavel <pavel.pakhomov.99@gmail.com>
2019-09-21 19:00:43 +0200

    negated terminals generation tests passed

--- src/main/java/com/intellij/xtextLanguage/xtext/generator/BnfGenerator.kt
+++ src/main/java/com/intellij/xtextLanguage/xtext/generator/BnfGenerator.kt
@@ -1,5 +1,6 @@
 package com.intellij.xtextLanguage.xtext.generator
 
+import com.intellij.psi.PsiElement
 import com.intellij.psi.util.PsiTreeUtil
 import com.intellij.xtextLanguage.xtext.psi.*
 import java.io.File
@@ -24,9 +25,13 @@ class BnfGenerator(val extention: String, val fileModel: XtextFileModel) {
     fun generate() {
         out = PrintWriter(FileOutputStream(myBnfFile))
 
+        out.print("{\n")
 
         generateTerminalRules()
         generateAttributes()
+
+        out.print("}\n")
+
         generateRules()
 //        generateEnumRules()
         generateReferences()
@@ -48,34 +53,34 @@ class BnfGenerator(val extention: String, val fileModel: XtextFileModel) {
 
     private fun generateAttributes() {
         out.print("""
-            
-  parserClass="com.intellij.xtextLanguage.xtext.parser.XtextParser"
-
-  extends="com.intellij.xtextLanguage.xtext.psi.impl.XtextPsiCompositeElementImpl"
-  psiClassPrefix="Xtext"
-  psiImplClassSuffix="Impl"
-  psiPackage="com.intellij.xtextLanguage.xtext.psi"
-  psiImplPackage="com.intellij.xtextLanguage.xtext.impl"
-
-  elementTypeHolderClass="com.intellij.xtextLanguage.xtext.psi.XtextTypes"
-  elementTypeClass="com.intellij.xtextLanguage.xtext.psi.XtextElementType"
-  tokenTypeClass="com.intellij.xtextLanguage.xtext.psi.XtextTokenType"
-  psiImplUtilClass="com.intellij.xtextLanguage.xtext.psi.impl.XtextPsiImplUtil"
-  parserUtilClass= "com.intellij.xtextLanguage.xtext.parserUtilBase.GeneratedParserUtilBaseCopy"
-  generateTokenAccessors=true
-  generateTokens=true
-
-}
-
-        """.trimIndent())
+          |    parserClass="com.intellij.xtextLanguage.xtext.parser.XtextParser"
+        
+          |    extends="com.intellij.xtextLanguage.xtext.psi.impl.XtextPsiCompositeElementImpl"
+          |    psiClassPrefix="Xtext"
+          |    psiImplClassSuffix="Impl"
+          |    psiPackage="com.intellij.xtextLanguage.xtext.psi"
+          |    psiImplPackage="com.intellij.xtextLanguage.xtext.impl"
+        
+          |    elementTypeHolderClass="com.intellij.xtextLanguage.xtext.psi.XtextTypes"
+          |    elementTypeClass="com.intellij.xtextLanguage.xtext.psi.XtextElementType"
+          |    tokenTypeClass="com.intellij.xtextLanguage.xtext.psi.XtextTokenType"
+          |    psiImplUtilClass="com.intellij.xtextLanguage.xtext.psi.impl.XtextPsiImplUtil"
+          |    parserUtilClass= "com.intellij.xtextLanguage.xtext.parserUtilBase.GeneratedParserUtilBaseCopy"
+          |    generateTokenAccessors=true
+          |    generateTokens=true
+                """.trimMargin("|"))
+        out.print("\n")
     }
 
     private fun generateTerminalRules() {
-        out.print("""{
-            |   tokens = [
-        """.trimMargin())
-        fileModel.myTerminalRules.forEach { out.print("      ${it.name} =\"regexp: ${getRegexpAsString(it.myRule)}\"\n") }
-        out.print("    ]")
+        out.print("    tokens = [\n")
+        fileModel.myTerminalRules.forEach {
+            if (getRegexpAsString(it.myRule) != "") {
+                out.print("      ${it.name} =\"regexp:${getRegexpAsString(it.myRule)}\"\n")
+            }
+
+        }
+        out.print("    ]\n")
     }
 
     fun getRuleAlternativesAsString(rule: XtextParserRule?): String {
@@ -95,7 +100,6 @@ class BnfGenerator(val extention: String, val fileModel: XtextFileModel) {
     }
 
     private fun writebrunch(b: XtextConditionalBranch, sb: StringBuilder): String {
-        if (b == null) return ""
         val unorderedGroup = b.getUnorderedGroup() ?: return ""
         val groups = unorderedGroup.getGroupList()
         val tokens = ArrayList<XtextAbstractToken>()
@@ -188,14 +192,14 @@ class BnfGenerator(val extention: String, val fileModel: XtextFileModel) {
     }
 
     fun getRegexpAsString(rule: XtextTerminalRule): String {
-        rule ?: return ""
         val sb = StringBuilder()
         var leaf = PsiTreeUtil.nextLeaf(rule.colon!!)
         loop@ while (leaf != null && PsiTreeUtil.isAncestor(rule, leaf, true)) {
             val type = leaf.node.elementType.toString()
-            val stringBuilder = when (type) {
+            when (type) {
                 "STRING" -> {
-                    var str = leaf.text.replace("\'", "")
+                    var str = leaf.text.substring(1, leaf.text.lastIndex)
+
                     str = avoidRegexpSymbols(str)
                     sb.append(str)
                 }
@@ -211,26 +215,102 @@ class BnfGenerator(val extention: String, val fileModel: XtextFileModel) {
                             leaf = PsiTreeUtil.nextLeaf(leaf)
                         }
                         if (leaf?.getNode()?.elementType.toString() == "STRING") {
-                            var str = leaf?.text?.replace("\'", "")
+                            var str = leaf?.text?.substring(1, leaf.text.lastIndex)
                             str = avoidRegexpSymbols(str ?: "")
                             sb.append("$str]")
-                        } else break@loop
-                    } else break@loop
+                        } else return ""
+                    } else return ""
 
                 }
                 "ID" -> sb.append("ref_${leaf.text}")
                 "!" -> {
-                    sb.clear()
-                    break@loop
+                    if (!writeNegatedRegexp(leaf, sb)) {
+                        return ""
+                    } else {
+                        val parent = leaf.parent?.parent?.parent?.parent
+                        if (parent != null) {
+                            leaf = PsiTreeUtil.nextLeaf(parent)
+                            continue@loop
+                        } else return ""
+                    }
+
                 }
+                "WHITE_SPACE" -> sb.append("")
                 ";" -> break@loop
-                else -> sb.append("")
+                else -> sb.append(leaf.text)
             }
             if (leaf != null) {
                 leaf = PsiTreeUtil.nextLeaf(leaf)
             }
         }
         return sb.toString()
+    }
+
+    private fun writeNegatedRegexp(leaf: PsiElement, sb: StringBuilder): Boolean {
+        sb.append("[^")
+        val sibling = leaf.nextSibling
+        if (sibling is XtextTerminalTokenElement) {
+            var b = false
+            sibling.characterRange?.let {
+
+                b = writeNegatedRegexpRange(it, sb)
+                sb.append("]")
+                if (!b) {
+                    sb.clear()
+                    return false
+                }
+                return true
+
+            }
+            sibling.parenthesizedTerminalElement?.terminalAlternatives?.terminalGroupList?.forEach {
+
+                if (it?.terminalTokenList?.size == 1) {
+                    if (it.terminalTokenList.first().asterisk == null && it.terminalTokenList.first().quesMark == null && it.terminalTokenList.first().plus == null && it.terminalTokenList.first().terminalTokenElement.characterRange != null) {
+                        it.terminalTokenList.first().terminalTokenElement.characterRange?.let { b = writeNegatedRegexpRange(it, sb) }
+
+                    } else {
+                        sb.clear()
+                        return false
+                    }
+                    if (!b) {
+                        sb.clear()
+                        return false
+                    }
+
+                } else {
+                    sb.clear()
+                    return false
+                }
+
+
+            }
+            if (b) {
+                sb.append("]")
+                return true
+            }
+
+        }
+        sb.clear()
+        return false
+    }
+
+    private fun writeNegatedRegexpRange(range: XtextCharacterRange, sb: StringBuilder): Boolean {
+
+        val list = range.keywordList
+        if (list.size == 1 && isOneCharacterString(list.first().text)) {
+            sb.append(avoidRegexpSymbols(list.first().text.substring(1, list.first().text.lastIndex)))
+            return true
+        } else if (list.size == 2 && isOneCharacterString(list.get(0).text) && isOneCharacterString(list.get(1).text) && range.range != null) {
+            sb.append("${avoidRegexpSymbols(list.first().text.substring(1, list.first().text.lastIndex))}-${avoidRegexpSymbols(list.get(1).text.replace("'", ""))}")
+            return true
+        }
+        return false
+    }
+
+    private fun isOneCharacterString(string: String): Boolean {
+        if (string.length == 3) return true
+        else if (string.length == 4 && string.contains('\\')) return true
+        else return false
     }
 
     private fun avoidRegexpSymbols(string: String): String {
