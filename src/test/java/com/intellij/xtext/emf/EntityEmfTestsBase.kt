899f175e0b13c286279f055de730e04f8ddb0f8b
Pavel <pavel.pakhomov.99@gmail.com>
2020-07-08 18:35:26 +0200

    absolute paths fix, persisting, loading, asserting methods added

--- src/test/java/com/intellij/xtext/emf/EntityEmfTestsBase.kt
+++ src/test/java/com/intellij/xtext/emf/EntityEmfTestsBase.kt
@@ -1,13 +1,20 @@
 package com.intellij.xtext.emf
 
+import com.intellij.entityLanguage.entity.emf.EntityEmfVisitor
+import com.intellij.entityLanguage.entity.psi.EntityDomainmodel
+import com.intellij.psi.util.PsiTreeUtil
 import com.intellij.testFramework.fixtures.BasePlatformTestCase
 import com.intellij.xtext.AllTests
+import junit.framework.TestCase
 import org.eclipse.emf.common.util.URI
 import org.eclipse.emf.ecore.EObject
 import org.eclipse.emf.ecore.resource.Resource
 import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl
 import org.eclipse.emf.ecore.xmi.impl.XMIResourceFactoryImpl
+import org.xtext.example.entity.entity.Domainmodel
 import org.xtext.example.entity.entity.EntityPackage
+import java.nio.file.Files
+import java.nio.file.Paths
 
 open class EntityEmfTestsBase(val myDataFolder: String) : BasePlatformTestCase() {
 
@@ -31,9 +38,45 @@ open class EntityEmfTestsBase(val myDataFolder: String) : BasePlatformTestCase()
         m.put("entity", XMIResourceFactoryImpl());
 
         val resSet = ResourceSetImpl();
-        val resource = resSet.getResource(URI.createURI("/Users/pavel/work/xtextGradle/XtextLanguageGradle/src/test/resources/testData/emf/" + fileName), true);
+        val resource = resSet.getResource(URI.createURI("$basePath/$fileName"), true);
 
         return resource.getContents().get(0);
+    }
+
+    fun getEntityEmfModel(): Domainmodel? {
+        val fileName = getCurrentInputFileName()
+        fileName?.let {
+            val file = myFixture.configureByFile(it)
+            val dm = PsiTreeUtil.findChildOfAnyType(file, EntityDomainmodel::class.java)
+            dm?.let {
+                EntityEmfVisitor.getEmfModel(it)?.let { return it }
+            }
+        }
+        return null
+    }
+
+    fun persistEntityEmfModel(model: EObject) {
+        val testFileName = getCurrentInputFileName()
+        EntityPackage.eINSTANCE.eClass()
+        val reg = Resource.Factory.Registry.INSTANCE
+        val m = reg.getExtensionToFactoryMap()
+        m.put("entity", XMIResourceFactoryImpl())
+        val resSet = ResourceSetImpl()
+
+        val resource = resSet.createResource(
+                URI.createFileURI(
+                        "$basePath/$testFileName"))
+
+        resource.getContents().add(model);
+        resource.save(null);
+    }
+
+
+    fun assertEqualXmi(compareWith: String) {
+        val testFileName = getCurrentInputFileName()
+        val myFileContent = Files.readAllLines(Paths.get("$basePath/$testFileName"))
+        val expectedContent = Files.readAllLines(Paths.get("$basePath/$compareWith"))
+        TestCase.assertTrue(myFileContent.equals(expectedContent))
     }
 
 }
